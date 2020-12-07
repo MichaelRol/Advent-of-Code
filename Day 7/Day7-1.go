41aaa32f9ff99631baa5f8dd4d5c873c4646f05d
MichaelRol <michael.rollins@hotmail.co.uk>
2020-12-07 21:26:24 +0000

    Day 7 Challenges Completed

---
+++ Day 7/Day7-1.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"log"
+	"io/ioutil"
+)
+
+func main() {
+
+	content, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	text := string(content)
+	lines := strings.Split(text, "\n")
+
+	rules := make(map[string][]string)
+	goldIn := make(map[string]bool)
+	goldIn["shiny gold bag"] = true
+
+	for _, line := range lines {
+		both := strings.Split(line, " contain ")
+		contains := strings.Split(both[1], ", ")
+		for _, colour := range contains {
+			if colour[len(colour)-1] == '.' {
+				colour = colour[:len(colour)-1]
+			}
+			if colour[len(colour)-1] == 's' {
+				colour = colour[:len(colour)-1]
+			}
+			rules[both[0][:len(both[0])-1]] = append(rules[both[0][:len(both[0])-1]], colour[2:])
+		}
+	}
+	for bag := range rules {
+		if lookInside(bag, &rules, &goldIn){
+			goldIn[bag] = true
+		}
+	}
+	
+	fmt.Println(len(goldIn) - 1)
+
+
+}
+
+func lookInside(bag string, rules *map[string][]string, goldIn *map[string]bool) bool {
+	if (*goldIn)[bag] {
+		return true
+	}
+	for _, eachBag := range((*rules)[bag]) {
+		if lookInside(eachBag, rules, goldIn) {
+			(*goldIn)[eachBag] = true
+			return true
+		}
+	}
+	return false
+}
---
+++ Day 7/Day7-2.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"log"
+	"io/ioutil"
+	"strconv"
+	"os"
+)
+
+func main() {
+
+	content, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	text := string(content)
+	lines := strings.Split(text, "\n")
+
+	rules := make(map[string][][]string)
+
+	for _, line := range lines {
+		both := strings.Split(line, " contain ")
+		contains := strings.Split(both[1], ", ")
+		for _, colour := range contains {
+			if colour[len(colour)-1] == '.' {
+				colour = colour[:len(colour)-1]
+			}
+			if colour[len(colour)-1] == 's' {
+				colour = colour[:len(colour)-1]
+			}
+			var numAndName []string
+			if colour[:1] == "n"{
+				numAndName = append(numAndName, "0")
+				numAndName = append(numAndName, "no other bags")
+			} else {
+				numAndName = append(numAndName, colour[:1])
+				numAndName = append(numAndName, colour[2:])
+			}
+			
+			rules[both[0][:len(both[0])-1]] = append(rules[both[0][:len(both[0])-1]], numAndName)
+		}
+	}
+
+	var numAndName []string
+	numAndName = append(numAndName, "1")
+	numAndName = append(numAndName, "shiny gold bag")
+	fmt.Println(countBags(numAndName, &rules) - 1)
+
+
+}
+
+
+func countBags(bag []string, rules *map[string][][]string) int {
+
+	total := 0
+	if bag[0] == "0" {
+		return 0
+	}
+	
+	for _, nextBag := range (*rules)[bag[1]] {
+		
+		i, err := strconv.Atoi(nextBag[0])
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(2)
+		}
+		
+		total = total + i * countBags(nextBag, rules)
+	}
+	
+	return 1 + total
+}
