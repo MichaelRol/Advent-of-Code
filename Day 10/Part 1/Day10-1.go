4b5de6289523f4ea8c0ea0d122c3c82a625c0416
MichaelRol <michael.rollins@hotmail.co.uk>
2020-12-10 16:53:45 +0000

    Day 10 Challenge Completed

---
+++ Day 10/Part 1/Day10-1.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"io/ioutil"
+	"strings"
+	"log"
+	"strconv"
+	"sort"
+)
+
+func main() {
+	
+	ratings := readNumbers("../input.txt")
+
+	sort.Ints(ratings)
+	ratings = append(ratings, ratings[len(ratings)-1] + 3)
+
+	oneJumps := 0
+	threeJumps := 0
+	if ratings[0] == 1 {
+		oneJumps++
+	} else if ratings[0] == 3 {
+		threeJumps++
+	}
+
+	for i := 1; i<len(ratings); i++ {
+		if ratings[i] - ratings[i-1] == 1 {
+			oneJumps++
+		} else if  ratings[i] - ratings[i-1] == 3 {
+			threeJumps++
+		}
+	}
+	fmt.Println(oneJumps*threeJumps)
+
+}
+
+func readNumbers(filename string) []int {
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(2)
+	}
+
+	text := string(content)
+	lines := strings.Split(text, "\n")
+
+	var numbers []int
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+		i, err := strconv.Atoi(line)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(2)
+		}
+		numbers = append(numbers, i)
+	}
+
+	return numbers
+}
---
+++ Day 10/Part 2/Day10-2.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"io/ioutil"
+	"strings"
+	"log"
+	"strconv"
+	"sort"
+)
+
+func main() {
+	
+	ratings := readNumbers("../input.txt")
+
+	ratings = append(ratings, 0)
+	sort.Ints(ratings)
+	ratings = append(ratings, ratings[len(ratings)-1] + 3)
+
+	oneJumps := 0
+	threeJumps := 0
+	
+	if ratings[0] == 1 {
+		oneJumps++
+	} else if ratings[0] == 3 {
+		threeJumps++
+	}
+
+	for i := 1; i<len(ratings); i++ {
+		if ratings[i] - ratings[i-1] == 1 {
+			oneJumps++
+		} else if  ratings[i] - ratings[i-1] == 3 {
+			threeJumps++
+		} 
+	}
+	graph := createGraph(ratings)
+	cache := make(map[int]int64)
+
+	fmt.Println(transverse(ratings[0], graph, cache))
+
+}
+
+func transverse(node int, graph map[int][]int, cache map[int]int64) int64 {
+
+	var count int64 = 0
+	if len(graph[node]) == 0 {
+		return 1
+	}
+	for _, nextNode := range(graph[node]) {
+		if cache[nextNode] == 0 {
+			count += transverse(nextNode, graph,  cache)
+		} else {
+			count += cache[nextNode]
+		}
+	}
+	cache[node] = count
+
+	return count
+}
+
+func createGraph(ratings []int) map[int][]int {
+
+	graph := make(map[int][]int)
+
+	for i, rating := range(ratings) {
+		var nexts []int
+		for _, j := range []int{1, 2, 3}{
+			if i+j < len(ratings) {
+				if ratings[i + j] - rating <= 3{
+					nexts = append(nexts, ratings[i+j])
+				}
+			}
+		}
+		graph[rating] = nexts
+	}
+
+	return graph	
+}
+
+func readNumbers(filename string) []int {
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(2)
+	}
+
+	text := string(content)
+	lines := strings.Split(text, "\n")
+
+	var numbers []int
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+		i, err := strconv.Atoi(line)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(2)
+		}
+		numbers = append(numbers, i)
+	}
+
+	return numbers
+}
