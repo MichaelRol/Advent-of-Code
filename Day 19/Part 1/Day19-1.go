a06df52115f8ee6ad70bc819ba24616d1e04f858
MichaelRol <michael.rollins@hotmail.co.uk>
2020-12-19 20:32:05 +0000

    Started day 19 challenges, need finishing

---
+++ Day 19/Part 1/Day19-1.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	rules, _ := readInput("../input.txt")
+
+	expandedRule := expandRule(rules["0"], rules)
+	fmt.Println(expandedRule)
+
+}
+
+func expandRule(rule [][]string, rules map[string][][]string) [][]string {
+	var newRule [][]string
+
+	for _, subRule := range rules {
+		newRule = append(newRule, expandRule(subRule, rules))
+	}
+
+	return newRule
+}
+
+func readInput(filename string) (rules map[string][][]string, messages []string) {
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(2)
+	}
+
+	text := string(content)
+	sections := strings.Split(text, "\n\n")
+
+	ruleStrings := strings.Split(sections[0], "\n")
+	rules = make(map[string][][]string)
+
+	for _, rule := range ruleStrings {
+		splitRule := strings.Split(rule, ": ")
+		subRules := strings.Split(splitRule[1], " | ")
+		for _, subRule := range subRules {
+			rules[splitRule[0]] = append(rules[splitRule[0]], strings.Split(subRule, " "))
+		}
+	}
+
+	messages = strings.Split(sections[1], "\n")
+
+	return rules, messages
+}
