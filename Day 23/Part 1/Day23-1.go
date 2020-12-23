44a1e31fe074c31c0a5d20244851f5af109b300c
MichaelRol <michael.rollins@hotmail.co.uk>
2020-12-23 14:37:03 +0000

    Day 23 Challenges Complete - Part 1 vastly over complicated

---
+++ Day 23/Part 1/Day23-1.go
@@ -0,0 +1,222 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type game struct {
+	current *cup
+	head    *cup
+	tail    *cup
+	highVal int
+	lowVal  int
+}
+
+type cup struct {
+	value int
+	next  *cup
+	prev  *cup
+}
+
+func main() {
+	input := "643719258"
+	var inputArray []int
+	for _, num := range input {
+		inputArray = append(inputArray, stringToInt(string(num)))
+	}
+	game := createGame()
+	for _, num := range inputArray {
+		game.addCup(num)
+	}
+	game.current = game.head
+	for i := 0; i < 100; i++ {
+		game.playRound()
+	}
+	game.listCupsAfter1()
+}
+
+func (g *game) playRound() {
+	numOfCups := 3
+	pickUp := g.pickUpCups(numOfCups)
+	dest := g.findDest()
+	for i := numOfCups - 1; i >= 0; i-- {
+		g.insertCupAfter(pickUp[i], dest)
+	}
+	g.nextCup()
+}
+
+func (g *game) listCupsAfter1() {
+	cup := g.findCup(1).next
+	for cup.value != 1 {
+		fmt.Print(cup.value)
+		cup = cup.next
+	}
+	fmt.Println()
+}
+
+func (g *game) pickUpCups(count int) []*cup {
+	var pickedUpCups []*cup
+	for i := 0; i < count; i++ {
+		cup := g.removeCup(g.current.next)
+		pickedUpCups = append(pickedUpCups, cup)
+	}
+	return pickedUpCups
+}
+
+func (g *game) insertCupAfter(c, dest *cup) {
+	c.next = dest.next
+	c.prev = dest
+	dest.next.prev = c
+	dest.next = c
+	if c.value > g.highVal {
+		g.highVal = c.value
+	}
+	if c.value < g.lowVal {
+		g.lowVal = c.value
+	}
+	if dest == g.tail {
+		g.tail = c
+	}
+}
+
+func (g *game) findDest() *cup {
+	destValue := g.current.value - 1
+	for g.findCup(destValue) == nil {
+		if destValue <= g.lowVal {
+			destValue = g.highVal
+		} else {
+			destValue--
+		}
+	}
+	destCup := g.findCup(destValue)
+	return destCup
+}
+
+func (g *game) nextCup() *cup {
+	g.current = g.current.next
+	return g.current
+}
+
+// func (g *game) prevCup() *cup {
+// 	g.current = g.current.prev
+// 	return g.current
+// }
+
+func (g *game) findCup(value int) *cup {
+	currentCup := g.head
+	for {
+		if currentCup.value == value {
+			return currentCup
+		}
+		if currentCup == g.tail {
+			break
+		}
+		currentCup = currentCup.next
+	}
+	return nil
+}
+
+func createGame() *game {
+	return &game{}
+}
+
+func (g *game) addCup(value int) {
+	c := &cup{value: value}
+	if g.head == nil {
+		g.head = c
+		g.lowVal = c.value
+		g.highVal = c.value
+	} else {
+		currentCup := g.tail
+		currentCup.next = c
+		c.prev = g.tail
+		if c.value > g.highVal {
+			g.highVal = c.value
+		}
+		if c.value < g.lowVal {
+			g.lowVal = c.value
+		}
+	}
+	g.tail = c
+	c.next = g.head
+	g.head.prev = c
+}
+
+func (g *game) removeCup(c *cup) *cup {
+	c.prev.next = c.next
+	c.next.prev = c.prev
+	if c == g.head {
+		g.head = c.next
+	}
+	if c == g.tail {
+		g.tail = c.prev
+	}
+	if c.value == g.highVal {
+		g.highVal = g.findHighVal()
+	}
+	if c.value == g.lowVal {
+		g.lowVal = g.findLowVal()
+	}
+
+	c.next = nil
+	c.prev = nil
+	return c
+}
+
+func (g *game) findHighVal() int {
+	max := 0
+	for _, value := range g.listAllValues() {
+		if value > max {
+			max = value
+		}
+	}
+	return max
+}
+
+func (g *game) findLowVal() int {
+	min := 9999999
+	for _, value := range g.listAllValues() {
+		if value < min {
+			min = value
+		}
+	}
+	return min
+}
+
+func (g *game) listAllValues() []int {
+	currentCup := g.head
+	var values []int
+	if currentCup == nil {
+		fmt.Println("Playlist is empty.")
+		return []int{}
+	}
+	values = append(values, currentCup.value)
+	for currentCup != g.tail {
+		currentCup = currentCup.next
+		values = append(values, currentCup.value)
+	}
+	return values
+}
+
+// func (g *game) showAllCups() {
+// 	currentCup := g.head
+// 	if currentCup == nil {
+// 		fmt.Println("Playlist is empty.")
+// 		return
+// 	}
+// 	fmt.Print(currentCup.value)
+// 	for currentCup != g.tail {
+// 		currentCup = currentCup.next
+// 		fmt.Print(currentCup.value)
+// 	}
+// 	fmt.Println()
+// }
+
+func stringToInt(input string) int {
+	num, err := strconv.Atoi(input)
+	if err != nil {
+		panic(err)
+	}
+	return num
+}
---
+++ Day 23/Part 2/Day23-2.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	input := "643719258"
+	var cups []int
+	nextCup := make(map[int]int)
+	for _, num := range input {
+		cups = append(cups, stringToInt(string(num)))
+	}
+	for i := 10; i < 1000001; i++ {
+		cups = append(cups, i)
+	}
+	numOfCups := len(cups)
+	for i, cup := range cups {
+		if cup == numOfCups {
+			nextCup[cup] = cups[0]
+		} else {
+			nextCup[cup] = cups[i+1]
+		}
+	}
+	currentCup := cups[0]
+
+	for i := 0; i < 10000000; i++ {
+		nextCup = playRound(currentCup, numOfCups, nextCup)
+		currentCup = nextCup[currentCup]
+	}
+
+	num1 := nextCup[1]
+	num2 := nextCup[nextCup[1]]
+	fmt.Println(num1 * num2)
+}
+
+func playRound(currentCup, numOfCups int, nextCup map[int]int) map[int]int {
+	cup1 := nextCup[currentCup]
+	cup2 := nextCup[cup1]
+	cup3 := nextCup[cup2]
+	dest := currentCup - 1
+	if dest == 0 {
+		dest = numOfCups
+	}
+	for dest == cup1 || dest == cup2 || dest == cup3 {
+		dest--
+		if dest == 0 {
+			dest = numOfCups
+		}
+	}
+	nextCup[currentCup] = nextCup[cup3]
+	nextCup[cup3] = nextCup[dest]
+	nextCup[dest] = cup1
+	return nextCup
+}
+
+func stringToInt(input string) int {
+	num, err := strconv.Atoi(input)
+	if err != nil {
+		panic(err)
+	}
+	return num
+}
