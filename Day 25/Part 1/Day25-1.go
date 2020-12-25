ff22ccaae9bc1a557de186bb7503ab530797879c
MichaelRol <michael.rollins@hotmail.co.uk>
2020-12-25 12:44:26 +0000

    Day 25 Challenge Completed. AOC2020 Completed.

---
+++ Day 25/Part 1/Day25-1.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	cardPK, doorPK := readInput("../input.txt")
+	doorLoopSize := calcLoopSize(7, doorPK)
+
+	encryptionKey := transformSubject(cardPK, doorLoopSize)
+	t := time.Now()
+
+	fmt.Print("Answer: ")
+	fmt.Println(encryptionKey)
+
+	fmt.Print("Execution time: ")
+	elapsed := t.Sub(start)
+	fmt.Println(elapsed)
+}
+
+func transformSubject(subject, loopSize int) int {
+	value := 1
+	divisor := 20201227
+	for i := 0; i < loopSize; i++ {
+		value *= subject
+		value %= divisor
+	}
+	return value
+}
+
+func calcLoopSize(subject, publicKey int) int {
+	value := 1
+	divisor := 20201227
+
+	for i := 1; ; i++ {
+		value *= subject
+		value %= divisor
+		if value == publicKey {
+			return i
+		}
+	}
+}
+
+func readInput(filename string) (cardPK, doorPK int) {
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(2)
+	}
+	text := string(content)
+	lines := strings.Split(text, "\n")
+
+	cardPK, err = strconv.Atoi(lines[0])
+	if err != nil {
+		panic(err)
+	}
+	doorPK, err = strconv.Atoi(lines[1])
+	if err != nil {
+		panic(err)
+	}
+
+	return cardPK, doorPK
+}
