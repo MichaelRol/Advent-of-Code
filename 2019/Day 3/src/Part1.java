fd9c315dda9866119497fa65f493209aac90b726
MichaelRol <michael.rollins@hotmail.co.uk>
2021-05-11 22:55:28 +0100

    2019. Day 3 - Part 1 - Complete <1s

--- 2019/Day 3/src/Part1.java
+++ 2019/Day 3/src/Part1.java
@@ -1,9 +1,9 @@
 import java.util.List;
-import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 public class Part1 {
 	public static int run(List<String> wire1str, List<String> wire2str) {
-		ArrayList<int[]> wire1 = new ArrayList<int[]>();
-		ArrayList<int[]> wire2 = new ArrayList<int[]>();
+		Map<String, Integer> wire1 = new HashMap<String, Integer>();
 		
 		int[] wire1curr = {0, 0};
 		int[] wire2curr = {0, 0};
@@ -11,22 +11,22 @@ public class Part1 {
 			if (cmd.charAt(0) == 'U') {
 				for (int i = 0; i < Integer.parseInt(cmd.substring(1, cmd.length())); i++) {
 					wire1curr[1]++;
-					wire1.add(wire1curr.clone());
+					wire1.put(wire1curr[0] + " " + wire1curr[1], Math.abs(wire1curr[0]) + Math.abs(wire1curr[1]));
 				}
 			} else if (cmd.charAt(0) == 'D') {
 				for (int i = 0; i < Integer.parseInt(cmd.substring(1, cmd.length())); i++) {
 					wire1curr[1]--;
-					wire1.add(wire1curr.clone());
+					wire1.put(wire1curr[0] + " " + wire1curr[1], Math.abs(wire1curr[0]) + Math.abs(wire1curr[1]));
 				}
 			} else if (cmd.charAt(0) == 'R') {
 				for (int i = 0; i < Integer.parseInt(cmd.substring(1, cmd.length())); i++) {
 					wire1curr[0]++;
-					wire1.add(wire1curr.clone());
+					wire1.put(wire1curr[0] + " " + wire1curr[1], Math.abs(wire1curr[0]) + Math.abs(wire1curr[1]));
 				}
 			} else if (cmd.charAt(0) == 'L') {
 				for (int i = 0; i < Integer.parseInt(cmd.substring(1, cmd.length())); i++) {
 					wire1curr[0]--;
-					wire1.add(wire1curr.clone());
+					wire1.put(wire1curr[0] + " " + wire1curr[1], Math.abs(wire1curr[0]) + Math.abs(wire1curr[1]));
 				}
 			}
 		}
@@ -35,36 +35,41 @@ public class Part1 {
 			if (cmd.charAt(0) == 'U') {
 				for (int i = 0; i < Integer.parseInt(cmd.substring(1, cmd.length())); i++) {
 					wire2curr[1]++;
-					wire2.add(wire2curr.clone());
+					if (wire1.containsKey(wire2curr[0] + " " + wire2curr[1])) {
+						if (wire1.get(wire2curr[0] + " " + wire2curr[1]) < min) {
+							min = wire1.get(wire2curr[0] + " " + wire2curr[1]);
+						}
+					}
 				}
 			} else if (cmd.charAt(0) == 'D') {
 				for (int i = 0; i < Integer.parseInt(cmd.substring(1, cmd.length())); i++) {
 					wire2curr[1]--;
-					wire2.add(wire2curr.clone());
+					if (wire1.containsKey(wire2curr[0] + " " + wire2curr[1])) {
+						if (wire1.get(wire2curr[0] + " " + wire2curr[1]) < min) {
+							min = wire1.get(wire2curr[0] + " " + wire2curr[1]);
+						}
+					}
 				}
 			} else if (cmd.charAt(0) == 'R') {
 				for (int i = 0; i < Integer.parseInt(cmd.substring(1, cmd.length())); i++) {
 					wire2curr[0]++;
-					wire2.add(wire2curr.clone());
+					if (wire1.containsKey(wire2curr[0] + " " + wire2curr[1])) {
+						if (wire1.get(wire2curr[0] + " " + wire2curr[1]) < min) {
+							min = wire1.get(wire2curr[0] + " " + wire2curr[1]);
+						}
+					}
 				}
 			} else if (cmd.charAt(0) == 'L') {
 				for (int i = 0; i < Integer.parseInt(cmd.substring(1, cmd.length())); i++) {
 					wire2curr[0]--;
-					wire2.add(wire2curr.clone());
-				}
-			}
-			
-		}
-		
-		for (int x = 0; x < wire1.size(); x++) {
-			for (int y = 0; y < wire2.size(); y++) {
-				if (wire1.get(x)[0] == wire2.get(y)[0] && wire1.get(x)[1] == wire2.get(y)[1]) {
-//					System.out.println(wire1.get(x)[0] + ", " + wire1.get(x)[1]);
-					if (Math.abs(wire1.get(x)[0]) + Math.abs(wire1.get(x)[1]) < min) {
-						min = Math.abs(wire1.get(x)[0]) + Math.abs(wire1.get(x)[1]); 
+					if (wire1.containsKey(wire2curr[0] + " " + wire2curr[1])) {
+						if (wire1.get(wire2curr[0] + " " + wire2curr[1]) < min) {
+							min = wire1.get(wire2curr[0] + " " + wire2curr[1]);
+						}
 					}
 				}
 			}
+			
 		}
 		return min;
 	}
