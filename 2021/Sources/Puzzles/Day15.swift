dc8e754c50eba53dbbbd5f01b4a41c0d2f1d7219
Michael Rollins <MichaelRol@users.noreply.github.com>
2021-12-15 12:56:35 +0000

    AOC2021 - Day 15: Part 1 Complete

--- 2021/Sources/Puzzles/Day15.swift
+++ 2021/Sources/Puzzles/Day15.swift
@@ -31,36 +31,33 @@ public class Day15 : Puzzle {
             for y in 0...data[0].count - 1 {
                 lengths[x * (data.count) + y] = Int(data[x][y])!
                 dist[x * (data.count) + y] = Int.max
-                unvisited.append(x * (data.count) + y)
                 prev[x * (data.count) + y] = nil
             }
         }
         dist[0] = 0
+        unvisited.append(0)
         lengths[0] = 0
+        var u = 0
         while unvisited.count != 0 {
-            var u: (Int, Int) = (0, Int.max)
-            for x in unvisited {
-                if dist[x]! < u.1 {
-                    u = (x, dist[x]!)
-                }
-            }
-            
-            print("count" + String(unvisited.count))
-            print(u.0)
-            unvisited = unvisited.filter{$0 != u.0}
+            unvisited = unvisited.filter{$0 != u}
             let neighs = [-1, 1, data.count, -(data.count)]
             for neigh in neighs {
-                if (neigh == 1 && u.0 % data.count == data.count - 1) || (neigh == -1 && u.0 % data.count == 0) {
+                if (neigh == 1 && u % data.count == data.count - 1) || (neigh == -1 && u % data.count == 0)
+                    || (neigh == data.count && u >= data.count * (data.count - 1)) || (neigh == -data.count && u < data.count) {
                     continue
                 }
-                if unvisited.filter({$0 == u.0+neigh}).count == 1 {
-                    let neighbour = unvisited.filter({$0 == u.0+neigh})[0]
-                    let newLength = u.1 + lengths[neighbour]!
-                    if newLength < dist[neighbour]! {
-                        dist[neighbour] = newLength
-                        prev[neighbour] = u.0
-                    }
+                let neighbour = u+neigh
+                let newLength = dist[u]! + lengths[neighbour]!
+                if newLength < dist[neighbour]! {
+                    unvisited.append(neighbour)
+                    dist[neighbour] = newLength
+                    prev[neighbour] = u
                 }
+            }
+            if unvisited.count != 0 {
+                u = unvisited.min(by: { (a, b) -> Bool in
+                    return dist[a]! < dist[b]!
+                })!
             }
         }
         return String(dist[(data.count) ^^ 2 - 1]!)
