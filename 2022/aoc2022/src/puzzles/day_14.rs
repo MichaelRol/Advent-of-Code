741d70a22a7b1ff81ab1011a0d87187306f5134f
Michael Rollins <MichaelRol@users.noreply.github.com>
2022-12-14 23:26:41 +0000

    Fixing run time on Day 14

--- 2022/aoc2022/src/puzzles/day_14.rs
+++ 2022/aoc2022/src/puzzles/day_14.rs
@@ -1,109 +1,127 @@
 use std::fs;
-use std::collections::HashSet;
 
 use itertools::Itertools;
 
 pub fn part1(path: String) -> i32 {
     let contents = fs::read_to_string(path)
         .expect("Should have been able to read the file");
-    let (mut rock, lowest) = produce_rock(contents);
+    let (mut rock, highest_y, lowest_x) = produce_rocks_1(contents);
     let mut count = 0;
     loop {
-        let mut sand: (u16, u16) = (500, 0);
+        let mut sand: (usize, usize) = (0, 500 - lowest_x,);
         loop {
-            let new_pos = next_position(sand, rock.clone());
-            if new_pos == sand {
-                rock.insert(new_pos);
+            if rock[sand.0 + 1][sand.1] == '.' {
+                sand =  (sand.0 + 1, sand.1);
+            } else if rock[sand.0 + 1][sand.1 - 1] == '.' {
+                sand =  (sand.0 + 1, sand.1 - 1);
+            } else if rock[sand.0 + 1][sand.1 + 1] == '.' {
+                sand =  (sand.0 + 1, sand.1 + 1);
+            } else {
+                rock[sand.0][sand.1] = 'X';
                 break;
             }
-            sand = new_pos;
-            if new_pos.1 > lowest {
+            if sand.0 >= highest_y {
                 return count;
             }
         }
         count += 1;
     }
+
 }
 
 pub fn part2(path: String) -> i32 {    
     let contents = fs::read_to_string(path)
         .expect("Should have been able to read the file");
-    let (mut rock, mut lowest) = produce_rock(contents);
-    lowest += 2;
-    for x in 0..1000 {
-        rock.insert((x, lowest));
-    }
-    let mut count = 0;
-    loop {
-        let mut sand: (u16, u16) = (500, 0);
+        let (mut rock, _, lowest_x) = produce_rocks_2(contents);
+        let mut count = 0;
         loop {
-            let new_pos = next_position(sand, rock.clone());
-            if new_pos == sand {
-                if sand == (500, 0) {
-                    return count + 1;
+            let mut sand: (usize, usize) = (0, 500 - lowest_x,);
+            loop {
+                if rock[sand.0 + 1][sand.1] == '.' {
+                    sand =  (sand.0 + 1, sand.1);
+                } else if rock[sand.0 + 1][sand.1 - 1] == '.' {
+                    sand =  (sand.0 + 1, sand.1 - 1);
+                } else if rock[sand.0 + 1][sand.1 + 1] == '.' {
+                    sand =  (sand.0 + 1, sand.1 + 1);
+                } else {
+                    if sand == (0, 500 -lowest_x) {
+                        return count + 1;
+                    }
+                    rock[sand.0][sand.1] = 'X';
+                    break;
                 }
-                rock.insert(new_pos);
-                break;
             }
-            sand = new_pos;
+            count += 1;
         }
-        count += 1;
-    }
 }
 
-fn next_position(sand: (u16, u16), rock: HashSet<(u16, u16)>) -> (u16, u16) {
-    if !rock.contains(&(sand.0, sand.1 + 1)) {
-        return (sand.0, sand.1 + 1);
-    }
-    if !rock.contains(&(sand.0 - 1, sand.1 + 1)) {
-        return (sand.0 - 1, sand.1 + 1);
-    }
-    if !rock.contains(&(sand.0 + 1, sand.1 + 1)) {
-        return (sand.0 + 1, sand.1 + 1);
-    }
-    return sand;
+fn produce_rocks_1(contents: String) -> (Vec<Vec<char>>, usize, usize) {
+    let highest_y = contents.lines()
+    .map(|line| line.split(" -> ")
+        .map(|pair| pair.split(",").last().unwrap().parse::<usize>().unwrap())
+        .max().unwrap())
+    .max().unwrap();
+    let highest_x = contents.lines()
+    .map(|line| line.split(" -> ")
+        .map(|pair| pair.split(",").next().unwrap().parse::<usize>().unwrap())
+        .max().unwrap())
+    .max().unwrap() + 1;
+    let lowest_x = contents.lines()
+    .map(|line| line.split(" -> ")
+        .map(|pair| pair.split(",").next().unwrap().parse::<usize>().unwrap())
+        .min().unwrap())
+    .min().unwrap() - 1;
+    let mut vec = vec![vec!['.'; (highest_x - lowest_x) + 1]; highest_y + 1];
+    add_rocks(contents, lowest_x, &mut vec);
+    return (vec, highest_y, lowest_x);
 }
 
-fn produce_rock(contents: String) -> (HashSet<(u16, u16)>, u16) {
-    let mut rock: HashSet<(u16, u16)> = HashSet::new();
-    let mut lowest = 0;
+fn produce_rocks_2(contents: String) -> (Vec<Vec<char>>, usize, usize) {
+    let highest_y = contents.lines()
+    .map(|line| line.split(" -> ")
+        .map(|pair| pair.split(",").last().unwrap().parse::<usize>().unwrap())
+        .max().unwrap())
+    .max().unwrap() + 2;
+    let highest_x = 500 + (highest_y);
+    let lowest_x = 500 - (highest_y);
+    let mut vec = vec![vec!['.'; (highest_x - lowest_x) + 1]; highest_y + 1];
+    add_rocks(contents, lowest_x, &mut vec);
+    for x in 0..=highest_x - lowest_x {
+        vec[highest_y][x] = 'X';
+    }
+    return (vec, highest_y, lowest_x);
+}
+
+fn add_rocks(contents: String, lowest_x: usize, vec: &mut Vec<Vec<char>>) {
     let rows = contents.lines()
         .map(|line| line.split(" -> ")
             .map(|pair| pair.split(",")
-                .map(|num| num.parse::<u16>().unwrap())
-                .next_tuple::<(u16, u16)>()).tuple_windows());
+                .map(|num| num.parse::<usize>().unwrap())
+                .next_tuple::<(usize, usize)>()).tuple_windows());
     for row in rows {
         for (a, b) in row {
             let first = a.unwrap();
             let last = b.unwrap();
-            if first.1 > lowest {
-                lowest = first.1;
-            }
-            if last.1 > lowest {
-                lowest = last.1;
-            }
             if first.0 > last.0 {
-                for x in last.0..=first.0 {
-                    rock.insert((x, first.1));
+                for x in last.0 - lowest_x..=first.0 - lowest_x {
+                    vec[first.1][x] = 'X';
                 }
             } else if first.0 < last.0 {
-                for x in first.0..=last.0 {
-                    rock.insert((x, first.1));
+                for x in first.0 - lowest_x..=last.0 - lowest_x {
+                    vec[first.1][x] = 'X';
                 }
             } else if first.1 > last.1 {
                 for y in last.1..=first.1 {
-                    rock.insert((first.0, y));
+                    vec[y][first.0 - lowest_x] = 'X';
                 }
             } else if first.1 < last.1 {
                 for y in first.1..=last.1 {
-                    rock.insert((first.0, y));
+                    vec[y][first.0 - lowest_x] = 'X';
                 }
             }
         }
     }
-    return (rock, lowest);
 }
-
 
 #[cfg(test)]
 mod test {
