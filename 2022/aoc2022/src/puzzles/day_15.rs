69cecdd74806d52d9e89eeae713ff210424dad25
Michael Rollins <MichaelRol@users.noreply.github.com>
2022-12-18 01:06:01 +0000

    Day 15: Part 2 complete

--- 2022/aoc2022/src/puzzles/day_15.rs
+++ 2022/aoc2022/src/puzzles/day_15.rs
@@ -1,4 +1,4 @@
-use std::{fs, num::ParseIntError, str::FromStr};
+use std::{fs, num::ParseIntError, str::FromStr, vec::IntoIter};
 
 use itertools::Itertools;
 
@@ -13,13 +13,51 @@ pub fn part1(path: String) -> i32 {
         .count() as i32;
 }
 
-pub fn part2(path: String) -> i32 {    
+pub fn part2(path: String) -> i64 {    
     let contents = fs::read_to_string(path)
     .expect("Should have been able to read the file");
 
-    return 0;
+    let sorted_sensors = contents.lines()
+        .map(|line| line.parse::<Sensor>().unwrap())
+        .sorted_by(|a, b| a.x.cmp(&b.x))
+        .collect::<Vec<Sensor>>();
+
+    for y in 0..=4000000 {
+        let ranges = sorted_sensors.iter()
+            .filter(|sensor| sensor.does_reach_y(y))
+            .map(|sensor| sensor.find_range_at_y(y))
+            .sorted_by(|a, b| a.0.cmp(&b.0));
+        let merged = merge_ranges(ranges);
+        if merged.len() > 1 {
+            return (merged[0].1 + 1) as i64 * 4000000 + y as i64;
+        }
+    }
+    return -1;
 }
 
+fn merge_ranges(ranges: IntoIter<(i32, i32)>) -> Vec<(i32, i32)> {
+    let mut new_ranges = Vec::new();
+    let mut min = 0;
+    let mut max = 0;
+    ranges.tuple_windows().for_each( |(ranges1, ranges2)| {
+        if ranges1.1 >= ranges2.0 - 1 || ranges1.1 < max {
+            if max < ranges2.1 {
+                max = ranges2.1
+            }
+            if max < ranges1.1 {
+                max = ranges1.1
+            }
+        } else {
+            new_ranges.push((min, max));
+            min = ranges2.0;
+            max = ranges2.1;
+        }
+    });
+    new_ranges.push((min, max));
+    return new_ranges;
+}
+
+#[derive(Clone)]
 struct Sensor {
     x: i32,
     y: i32,
@@ -35,8 +73,25 @@ impl Sensor {
 
     fn does_reach_y(&self, y: i32) -> bool {
         let dist = self.distance_from_beacon();
+        if self.y == y {
+            return true;
+        }
         return (self.y > y && self.y - dist < y) || (self.y < y && self.y + dist > y);
     }
+
+    fn find_range_at_y(&self, y: i32) -> (i32, i32) {
+        let dist_from_beacon = self.distance_from_beacon();
+        let dist_from_y = (self.y - y).abs();
+        let mut range_left = self.x - (dist_from_beacon - dist_from_y);
+        let mut range_right = self.x + (dist_from_beacon - dist_from_y);
+        if range_left < 0 {
+            range_left = 0;
+        }
+        if range_right > 4000000 {
+            range_right = 4000000;
+        }
+        return (range_left, range_right);
+    } 
 
     fn find_xs_a_y(&self, y: i32) -> Vec<i32> {
         let dist_from_beacon = self.distance_from_beacon();
@@ -79,11 +134,11 @@ mod test {
     
     #[test]
     fn test_part1() {
-        assert_eq!(part1("src/inputs/input15.txt".to_owned()), 0);
+        assert_eq!(part1("src/inputs/input15.txt".to_owned()), 4424278);
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2("src/inputs/input15.txt".to_owned()), 0);
+        assert_eq!(part2("src/inputs/input15.txt".to_owned()), 10382630753392);
     }
 }
