f2143951aa3b860666e22a9a9aee79138451bb0a
MichaelRol <michael.rollins@hotmail.co.uk>
2024-12-01 07:56:52 +0000

    Making Day 1 Part 2 speedier

--- 2024/days/day_01.ts
+++ 2024/days/day_01.ts
@@ -16,5 +16,9 @@ export function part2(rawInput: string) {
     const lists: string[][] = readChars(rawInput, "   ");
     const list1: number[] = lists.map(char => parseInt(char[0]));
     const list2: number[] = lists.map(char => parseInt(char[1]));
-    return list1.map(num => list2.filter(num2 => num2 === num).length * num).reduce((x, y) => x + y);
+    const list2Freq: Map<number, number> = new Map();
+    list2.forEach(num => list2Freq.set(num, (list2Freq.get(num) || 0) + 1));
+    
+    return list1.map(num => (list2Freq.get(num) || 0) * num).reduce((x, y) => x + y);
+    // return list1.map(num => list2.filter(num2 => num2 === num).length * num).reduce((x, y) => x + y);
 }
