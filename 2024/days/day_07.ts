5b16d725b9583b2a93959bff7652f07796a5b6d3
Michael Rollins <MichaelRol@users.noreply.github.com>
2024-12-08 09:19:42 +0100

    AOC2024 Day 7 - Part 2

--- 2024/days/day_07.ts
+++ 2024/days/day_07.ts
@@ -14,6 +14,20 @@ export function part1(rawInput: string) {
     return count;
 }
 
+export function part2(rawInput: string) {
+    const eqs = readChars(rawInput, ": ", x => x).map((line: string[]) => {
+        return {
+            target: parseInt(line[0]),
+            nums: line[1].split(" ").map(num => parseInt(num)).reverse()
+        };
+    });
+    let count = 0;
+    for (const eq of eqs) {
+        if (calc2(eq.target, eq.nums)) count += eq.target;
+    }
+    return count;
+}
+
 function calc(target: number, nums: number[]): boolean {
     if (nums.length === 1) {
         if (nums[0] === target) return true;
@@ -23,5 +37,21 @@ function calc(target: number, nums: number[]): boolean {
         return calc(target / nums[0], nums.slice(1)) || calc(target - nums[0], nums.slice(1));
     } else {
         return calc(target - nums[0], nums.slice(1));
+    }
+}
+
+function calc2(target: number, nums: number[]): boolean {
+    if (nums.length === 1) {
+        if (nums[0] === target) return true;
+        else return false;
+    }
+    const scale = nums[0].toString().length;
+    if (target % (10 ** scale) === nums[0]) {
+        return calc2(target / nums[0], nums.slice(1)) || calc2(target - nums[0], nums.slice(1)) || calc2(Math.floor(target / (10 ** scale)), nums.slice(1));
+    }
+    if (target % nums[0] === 0) {
+        return calc2(target / nums[0], nums.slice(1)) || calc2(target - nums[0], nums.slice(1));
+    } else {
+        return calc2(target - nums[0], nums.slice(1));
     }
 }
