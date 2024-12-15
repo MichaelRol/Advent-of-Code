e41693a4da1902dc94151d52d26b3ec68e67a1c5
Michael Rollins <MichaelRol@users.noreply.github.com>
2024-12-15 08:43:16 +0000

    AOC2024: Day 12 Enhancements

--- 2024/days/day_12.ts
+++ 2024/days/day_12.ts
@@ -3,176 +3,148 @@ import { readChars } from "../utils/input";
 
 export function part1(rawInput: string) {
     const map: string[][] = readChars(rawInput);
-    const visited: string[] = [];
+    const visited: Map<string, string[]> = new Map();
     let total = 0;
     for (let i = 0; i < map.length; i++) {
         for (let j = 0; j < map[0].length; j++) {
-            if (visited.includes(JSON.stringify([i, j]))) continue;
             const region = map[i][j];
+            if (visited.get(region)?.includes(JSON.stringify([i, j]))) continue;
             let toVisit: [number, number][] = [[i, j]];
+            let regionPositions: string[] = [JSON.stringify([i, j])];
             let area = 0;
             let perim = 0;
             while (toVisit.length > 0) {
                 const pos = toVisit.pop()!;
-                visited.push(JSON.stringify(pos));
+                regionPositions.push(JSON.stringify(pos));
                 area++;
-                if (map[pos[0] + 1] && map[pos[0] + 1][pos[1]] === region) {
-                    toVisit.push([pos[0] + 1, pos[1]]);
-                } else {
-                    perim++;
-                }
-                if (map[pos[0] - 1] && map[pos[0] - 1][pos[1]] === region) {
-                    toVisit.push([pos[0] - 1, pos[1]]);
-                } else {
-                    perim++;
-                }
-                if (map[pos[0]][pos[1] + 1] === region) {
-                    toVisit.push([pos[0], pos[1] + 1]);
-                } else {
-                    perim++;
-                }
-                if (map[pos[0]][pos[1] - 1] === region) {
-                    toVisit.push([pos[0], pos[1] - 1]);
-                } else {
-                    perim++;
-                }
-                toVisit = toVisit.filter(neighbor => !visited.includes(JSON.stringify(neighbor)));
+                perim += checkSquare(pos[0] + 1, pos[1], region, toVisit, map);
+                perim += checkSquare(pos[0] - 1, pos[1], region, toVisit, map);
+                perim += checkSquare(pos[0], pos[1] + 1, region, toVisit, map);
+                perim += checkSquare(pos[0], pos[1] - 1, region, toVisit, map);
+                toVisit = toVisit.filter(neighbor => !regionPositions.includes(JSON.stringify(neighbor)));
             }
+            visited.set(region, (visited.get(region) || []).concat(...regionPositions));
             total += area * perim;
         }
     }
     return total;
 }
+
 export function part2(rawInput: string) {
     const map: string[][] = readChars(rawInput);
-    const visited: string[] = [];
+    const visited: Map<string, string[]> = new Map();
     let total = 0;
     for (let i = 0; i < map.length; i++) {
         for (let j = 0; j < map[0].length; j++) {
-            if (visited.includes(JSON.stringify([i, j]))) continue;
             const region = map[i][j];
+            if (visited.get(region)?.includes(JSON.stringify([i, j]))) continue;
             let toVisit: [number, number][] = [[i, j]];
             let area = 0;
-            let nodes: [number, number][] = [];
+            let nodes: string[] = [];
             while (toVisit.length > 0) {
                 const pos = toVisit.pop()!;
-                visited.push(JSON.stringify(pos));
-                nodes.push(pos);
+                nodes.push(JSON.stringify(pos));
                 area++;
-                if (map[pos[0] + 1] && map[pos[0] + 1][pos[1]] === region) {
-                    toVisit.push([pos[0] + 1, pos[1]]);
-                    nodes.push([pos[0] + 1, pos[1]]);
-                }
-                if (map[pos[0] - 1] && map[pos[0] - 1][pos[1]] === region) {
-                    toVisit.push([pos[0] - 1, pos[1]]);
-                    nodes.push([pos[0] - 1, pos[1]]);
-                }
-                if (map[pos[0]][pos[1] + 1] === region) {
-                    toVisit.push([pos[0], pos[1] + 1]);
-                    nodes.push([pos[0], pos[1] + 1]);
-                }
-                if (map[pos[0]][pos[1] - 1] === region) {
-                    toVisit.push([pos[0], pos[1] - 1]);
-                    nodes.push([pos[0], pos[1] - 1]);
-                }
-                toVisit = toVisit.filter(neighbor => !visited.includes(JSON.stringify(neighbor)));
+                checkSquare(pos[0] + 1, pos[1], region, toVisit, map);
+                checkSquare(pos[0] - 1, pos[1], region, toVisit, map);
+                checkSquare(pos[0], pos[1] + 1, region, toVisit, map);
+                checkSquare(pos[0], pos[1] - 1, region, toVisit, map);
+                toVisit = toVisit.filter(neighbor => !nodes.includes(JSON.stringify(neighbor)));
             }
-            const uniqueNodes: [number, number][] = [];
-            const uniqueNodeStrings = new Set(nodes.map(node => JSON.stringify(node)));
-            uniqueNodeStrings.forEach(node => uniqueNodes.push(JSON.parse(node)));
-            const edges: number = calcNumOfEdges(uniqueNodes);
+            visited.set(region, (visited.get(region) || []).concat(...nodes));
+            const edges: number = calcNumOfEdges(nodes.map(node => JSON.parse(node)));
             total += area * edges;
         }
     }
     return total;
 }
 
+function checkSquare(i: number, j: number, region: string, toVisit: [number, number][], map: string[][]): number {
+    if (map[i] && map[i][j] === region) {
+        toVisit.push([i, j]);
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
 function calcNumOfEdges(nodes: [number, number][]): number {
     let edges = 0;
+    // TODO: Can we limit the range by creating a map of x => [yMin, yMax]
     const minX = nodes.map(node => node[0]).sort((a, b) => a - b)[0];
     const maxX = nodes.map(node => node[0]).sort((a, b) => a - b)[nodes.length - 1];
     const minY = nodes.map(node => node[1]).sort((a, b) => a - b)[0];
     const maxY = nodes.map(node => node[1]).sort((a, b) => a - b)[nodes.length - 1];
     // scan left to right
-    let scan = true;
     let ys: number[] = [];
     for (let x = minX; x <= maxX; x++) {
         const newYs: number[] = [];
-        for (let y = minY; y <= maxY; y++) {
-            if (nodes.find(node => _.isEqual(node, [x, y]))) {
-                if (scan) {
-                    if (!ys.includes(y)) {
-                        edges++;
-                    }
-                    newYs.push(y);
-                    scan = false;
-                }
-            } else {
-                if (!scan) scan = true;
-            }
+        let scan = false;
+        for (let y = minY; y <= maxY + 1; y++) {
+            ({ scan, edges } = checkForEdge(nodes, x, y, scan, ys, edges, newYs, true));
         }
         ys = newYs;
-        scan = true;
     }
-    // scan right to left
-    ys = [];
-    for (let x = minX; x <= maxX; x++) {
-        const newYs: number[] = [];
-        for (let y = maxY; y >= minY; y--) {
-            if (nodes.find(node => _.isEqual(node, [x, y]))) {
-                if (scan) {
-                    if (!ys.includes(y)) {
-                        edges++;
-                    }
-                    newYs.push(y);
-                    scan = false;
-                }
-            } else {
-                if (!scan) scan = true;
-            }
-        }
-        ys = newYs;
-        scan = true;
-    }
+    // // scan right to left
+    // ys = [];
+    // for (let x = minX; x <= maxX; x++) {
+    //     const newYs: number[] = [];
+    //     let scan = true;
+    //     for (let y = maxY; y >= minY; y--) {
+    //         ({ scan, edges } = checkForEdge(nodes, x, y, scan, ys, edges, newYs, true));
+    //     }
+    //     ys = newYs;
+    // }
     // scan top to bottom
     let xs: number[] = [];
     for (let y = minY; y <= maxY; y++) {
         const newXs: number[] = [];
-        for (let x = minX; x <= maxX; x++) {
-            if (nodes.find(node => _.isEqual(node, [x, y]))) {
-                if (scan) {
-                    if (!xs.includes(x)) {
-                        edges++;
-                    }
-                    newXs.push(x);
-                    scan = false;
-                }
-            } else {
-                if (!scan) scan = true;
-            }
+        let scan = false;
+        for (let x = minX; x <= maxX + 1; x++) {
+            ({ scan, edges } = checkForEdge(nodes, x, y, scan, xs, edges, newXs, false));
         }
         xs = newXs;
-        scan = true;
     }
-    // scan bottom to top
-    xs = [];
-    for (let y = minY; y <= maxY; y++) {
-        const newXs: number[] = [];
-        for (let x = maxX; x >= minX; x--) {
-            if (nodes.find(node => _.isEqual(node, [x, y]))) {
-                if (scan) {
-                    if (!xs.includes(x)) {
-                        edges++;
-                    }
-                    newXs.push(x);
-                    scan = false;
-                }
-            } else {
-                if (!scan) scan = true;
-            }
-        }
-        xs = newXs;
-        scan = true;
-    }
+    // // scan bottom to top
+    // xs = [];
+    // for (let y = minY; y <= maxY; y++) {
+    //     const newXs: number[] = [];
+    //     let scan = true;
+    //     for (let x = maxX; x >= minX; x--) {
+    //         ({ scan, edges } = checkForEdge(nodes, x, y, scan, xs, edges, newXs, false));
+    //     }
+    //     xs = newXs;
+    // }
     return edges;
+}
+
+function checkForEdge(
+    nodes: [number, number][],
+    x: number,
+    y: number,
+    inRegion: boolean,
+    xs: number[],
+    edges: number,
+    newXs: number[],
+    direction: boolean
+) {
+    if (nodes.find(node => _.isEqual(node, [x, y]))) {
+        if (!inRegion) {
+            if (!xs.includes(direction ? y : x)) {
+                edges++;
+            }
+            newXs.push(direction ? y : x);
+            // edges++;
+            inRegion = true;
+        }
+    } else {
+        if (inRegion) {
+            if (!xs.includes(direction ? y : x)) {
+                edges++;
+            }
+            newXs.push(direction ? y : x);
+            inRegion = false;
+        }
+    }
+    return { scan: inRegion, edges };
 }
