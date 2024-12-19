23df801137071685bb5372dd1eb97a79262aa5b5
Michael Rollins <MichaelRol@users.noreply.github.com>
2024-12-19 00:28:11 +0000

    AOC2024 Day 17 - Part 2. Had to take a hint

--- 2024/days/day_17.ts
+++ 2024/days/day_17.ts
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import { readLines } from "../utils/input";
 
 export function part1(rawInput: string) {
@@ -6,14 +7,50 @@ export function part1(rawInput: string) {
         .split(": ")[1]
         .split(",")
         .map(num => parseInt(num));
-    const regA = parseInt(lines[0].split(": ")[1]);
-    const regB = parseInt(lines[1].split(": ")[1]);
-    const regC = parseInt(lines[2].split(": ")[1]);
+    const regA = BigInt(parseInt(lines[0].split(": ")[1]));
+    const regB = BigInt(parseInt(lines[1].split(": ")[1]));
+    const regC = BigInt(parseInt(lines[2].split(": ")[1]));
 
     return runProgram(program, regA, regB, regC).join(",");
 }
 
-function runProgram(program: number[], regA: number, regB: number, regC: number): number[] {
+export function part2(rawInput: string) {
+    const lines = readLines(rawInput);
+    const program: number[] = lines[4]
+        .split(": ")[1]
+        .split(",")
+        .map(num => parseInt(num));
+    const regB = BigInt(parseInt(lines[1].split(": ")[1]));
+    const regC = BigInt(parseInt(lines[2].split(": ")[1]));
+    let result = BigInt(0);
+    const lastI = Array(program.length).fill(0);
+    for (let index = program.length - 1; index >= 0; index--) {
+        const target = program.slice(index);
+        let lastOutput: number[] = [];
+        for (let i = lastI[index]; i < 8; i++) {
+            const output = runProgram(program, result, regB, regC);
+            lastOutput = output;
+            if (_.isEqual(output, target)) {
+                if (index === 0) {
+                    return result;
+                }
+                result = result << BigInt(3);
+                lastI[index] = i;
+                break;
+            }
+            result++;
+        }
+        if (!_.isEqual(lastOutput, target)) {
+            index++;
+        }
+        if (index === 0 && !_.isEqual(lastOutput, program)) {
+            index++;
+        }
+    }
+    throw "Could not find solution";
+}
+
+function runProgram(program: number[], regA: bigint, regB: bigint, regC: bigint): number[] {
     const output: number[] = [];
     let pointer = 0;
     while (pointer < program.length) {
@@ -21,16 +58,16 @@ function runProgram(program: number[], regA: number, regB: number, regC: number)
         const operand = program[pointer + 1];
         switch (opcode) {
             case 0:
-                regA = Math.trunc(regA / 2 ** getCombo(operand, regA, regB, regC));
+                regA = regA / BigInt(2) ** getCombo(operand, regA, regB, regC);
                 break;
             case 1:
-                regB = regB ^ operand;
+                regB = regB ^ BigInt(operand);
                 break;
             case 2:
-                regB = getCombo(operand, regA, regB, regC) % 8;
+                regB = getCombo(operand, regA, regB, regC) % BigInt(8);
                 break;
             case 3:
-                if (regA !== 0) {
+                if (regA !== BigInt(0)) {
                     pointer = operand;
                     continue;
                 }
@@ -39,13 +76,13 @@ function runProgram(program: number[], regA: number, regB: number, regC: number)
                 regB = regB ^ regC;
                 break;
             case 5:
-                output.push(getCombo(operand, regA, regB, regC) % 8);
+                output.push(Number(getCombo(operand, regA, regB, regC) % BigInt(8)));
                 break;
             case 6:
-                regB = Math.trunc(regA / 2 ** getCombo(operand, regA, regB, regC));
+                regB = regA / BigInt(2) ** getCombo(operand, regA, regB, regC);
                 break;
             case 7:
-                regC = Math.trunc(regA / 2 ** getCombo(operand, regA, regB, regC));
+                regC = regA / BigInt(2) ** getCombo(operand, regA, regB, regC);
                 break;
             default:
                 throw "Invalid opcode: " + opcode;
@@ -55,13 +92,13 @@ function runProgram(program: number[], regA: number, regB: number, regC: number)
     return output;
 }
 
-function getCombo(operand: number, regA: number, regB: number, regC: number): number {
+function getCombo(operand: number, regA: bigint, regB: bigint, regC: bigint): bigint {
     switch (operand) {
         case 0:
         case 1:
         case 2:
         case 3:
-            return operand;
+            return BigInt(operand);
         case 4:
             return regA;
         case 5:
