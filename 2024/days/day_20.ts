8e0a890711f9a3fce28ea431211841d418886060
Michael Rollins <MichaelRol@users.noreply.github.com>
2024-12-24 15:40:34 +0000

    AOC2024 Day 20 - Part 2

--- 2024/days/day_20.ts
+++ 2024/days/day_20.ts
@@ -44,6 +44,43 @@ export function part1(rawInput: string) {
     return count;
 }
 
+export function part2(rawInput: string) {
+    let vertices: Map<string, [number, number][]> = new Map();
+    let distsForward: Map<string, number> = new Map();
+    let prevs: Map<string, string> = new Map();
+    let end: [number, number] | undefined;
+    let start: [number, number] | undefined;
+    const map: string[][] = readChars(rawInput);
+    ({ start, end } = populateVertices(map, vertices, distsForward));
+    map[end![0]][end![1]] = ".";
+    bfs(vertices, start!, distsForward, prevs);
+    let distsBackwards: Map<string, number> = new Map();
+    vertices.forEach((v, k) => distsBackwards.set(k, Number.MAX_VALUE));
+    bfs(vertices, end!, distsBackwards, new Map());
+
+    const road: string[] = [];
+    let backwards: string | undefined = JSON.stringify(end);
+    while (backwards) {
+        road.push(backwards);
+        backwards = prevs.get(backwards);
+    }
+    const baseDist = distsForward.get(JSON.stringify(end))!;
+
+    let count = 0;
+    for (const pos of road) {
+        const cheatEnds: { pos: [number, number]; dist: number }[] = findCheats(JSON.parse(pos), map);
+        for (const cheat of cheatEnds) {
+            const cheatPos = cheat.pos;
+            const cheatDist = cheat.dist;
+            const distTo = distsForward.get(pos)!;
+            const distFrom = distsBackwards.get(JSON.stringify(cheatPos))!;
+            const improvement = baseDist - (distFrom + distTo + cheatDist);
+            if (improvement >= (vertices.size < 100 ? 50 : 100)) count++;
+        }
+    }
+    return count;
+}
+
 function getCheatStartAndEnd(pos: [number, number], direction: string): [[number, number], [number, number]] {
     switch (direction) {
         case "N":
@@ -118,4 +155,20 @@ function populateVertices(map: string[][], vertices: Map<string, [number, number
         }
     }
     return { start, end };
+}
+
+function findCheats(pos: [number, number], map: string[][]): { pos: [number, number]; dist: number }[] {
+    const cheats: { pos: [number, number]; dist: number }[] = [];
+    for (let x = -20; x <= 20; x++) {
+        for (let y = -20 + Math.abs(x); y <= 20 + Math.abs(x); y++) {
+            const dist = Math.abs(x) + Math.abs(y);
+            if (map[pos[0] + x]?.[pos[1] + y] === "." && dist <= 20) {
+                cheats.push({
+                    pos: [pos[0] + x, pos[1] + y],
+                    dist: dist
+                });
+            }
+        }
+    }
+    return cheats;
 }
